93d4033a6b21c9fc209ec701b8ef16d70646eaa7
khairirafianwar <khairirafianwar@gmail.com>
2025-09-16 13:10:45 +0700

    Add files via upload

---
+++ Statement Elif.py
@@ -0,0 +1,10 @@
+nama = input("Siapa nama anda? ")
+
+if nama == "Asgun":
+    print("Wih, asgun rajin banget belajarnya!")
+elif nama == "Aji":
+    print("Halo Aji, kamu pintar sekali!")
+elif nama == "Andi":
+    print("Hai Andi, semangat terus ya!")
+else:
+    print("Hai, salam kenal!")
