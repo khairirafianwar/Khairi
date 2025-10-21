e0545fa6f47961da8717fe6f26d9757168b37dd3
khairirafianwar <khairirafianwar@gmail.com>
2025-10-21 12:46:34 +0700

    Add files via upload

---
+++ class.py
@@ -0,0 +1,19 @@
+#python 3.7.1
+#class
+
+class Sekolah :
+  pass
+ 
+
+sekolah1 = Sekolah()
+sekolah1.nama = "SMK PGRI 35 Solokan Jeruk"
+sekolah1.alamat = "JL RHO Kosasih No 99 Cibodas"
+
+sekolah2 = Sekolah()
+sekolah2.nama = "SMKN 9 Bandung"
+sekolah2.alamat = "JL Soekarno-Hatta No 10"
+
+print(sekolah1.nama)
+print(sekolah1.alamat)
+print(sekolah2.nama)
+print(sekolah2.alamat) 
