b3a95ee987577505cbf9cf8be5aca62ccf8746f2
khairirafianwar <khairirafianwar@gmail.com>
2025-10-22 14:30:12 +0700

    Add files via upload

---
+++ cl tugas 1.py
@@ -0,0 +1,30 @@
+class alatmusik:
+    pass
+
+alatmusik1 = alatmusik()
+alatmusik1.nama = "Biola"
+alatmusik1.jenis = "Gesek"
+alatmusik1.bahan = "Kayu dan senar"
+alatmusik1.warna = "Coklat"
+alatmusik1.asal = "Italia"
+
+alatmusik2 = alatmusik()
+alatmusik2.nama = "Gitar"
+alatmusik2.jenis = "Petik"
+alatmusik2.bahan = "Kayu"
+alatmusik2.warna = "Coklat muda"
+alatmusik2.asal = "Spanyol"
+
+print(alatmusik1.nama)
+print(alatmusik1.jenis)
+print(alatmusik1.bahan)
+print(alatmusik1.warna)
+print(alatmusik1.asal)
+
+print()
+
+print(alatmusik2.nama)
+print(alatmusik2.jenis)
+print(alatmusik2.bahan)
+print(alatmusik2.warna)
+print(alatmusik2.asal)
