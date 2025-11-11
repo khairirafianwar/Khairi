5ab45db921c9cbb5e570d47bb4bd77b1a1155d72
khairirafianwar <khairirafianwar@gmail.com>
2025-11-11 13:04:28 +0700

    Add files via upload

---
+++ cl tugas2.py
@@ -0,0 +1,29 @@
+class AlatMusik:
+    nama1 = "Biola"
+    jenis1 = "Gesek"
+    bahan1 = "Kayu dan senar"
+    warna1 = "Coklat"
+    asal1 = "Italia"
+
+    nama2 = "Gitar"
+    jenis2 = "Petik"
+    bahan2 = "Kayu"
+    warna2 = "Coklat"
+    asal2 = "Spanyol"
+
+alatmusik1 = AlatMusik()
+alatmusik2 = AlatMusik()
+
+print(alatmusik1.nama1)
+print(alatmusik1.jenis1)
+print(alatmusik1.bahan1)
+print(alatmusik1.warna1)
+print(alatmusik1.asal1)
+
+print()
+
+print(alatmusik2.nama2)
+print(alatmusik2.jenis2)
+print(alatmusik2.bahan2)
+print(alatmusik2.warna2)
+print(alatmusik2.asal2)
