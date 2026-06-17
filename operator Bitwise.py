37d3a8353a1a6a90a200b5ee48a7d648fcee7f38
khairirafianwar <khairirafianwar@gmail.com>
2026-06-17 10:29:31 +0700

    Add files via upload

---
+++ operator Bitwise.py
@@ -0,0 +1,41 @@
+#operator Bitwise
+
+a = 6
+b = 3
+
+#bitwise and (&)
+c = a & b
+print ('nilai :',a,)
+print ('nilai :',b,)
+print ('nilai Bitwise and:',c,)
+
+#bitwise or (|)
+c = a | b
+print ('nilai :',a,)
+print ('nilai :',b,)
+print ('nilai Bitwise or:',c,)
+
+#bitwise Xor (^)
+c = a ^ b
+print ('nilai :',a,)
+print ('nilai :',b,)
+print ('nilai Bitwise xor:',c,)
+
+#bitwise Not (~)
+c = ~a 
+c = ~b
+print ('nilai :',a,)
+print ('nilai :',b,)
+print ('nilai Bitwise not:',c,)
+
+#shift Right (>>)
+c = a >> 2
+print('nilai :',a,)
+print ('nilai :',b,)
+print ('nilai Bitwise shift right:',c,)
+
+#shift left (<<)
+c = a << 2
+print('nilai :',a,)
+print ('nilai :',b,)
+print ('nilai Bitwise shift left:',c,)
